64e17d81429fc6fcb97b8a94408f85755e892a66
yeongmo-j <yeongmoj94@gmail.com>
2019-09-01 04:35:56 +0900

    Add pgtid to userentity

--- netflix/src/main/java/com/example/netflix/aop/ResultAspect.java
+++ netflix/src/main/java/com/example/netflix/aop/ResultAspect.java
@@ -1,22 +1,23 @@
 package com.example.netflix.aop;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
-
-@Component
-@Aspect
+//@Aspect
 public class ResultAspect {
 /*
 	@AfterReturning("execution(* com..*(..))")
     public void success(JoinPoint joinPoint) throws Throwable {
-		HttpServletResponse response = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getResponse();
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+		ServletWebRequest servletWebRequest=new ServletWebRequest(request);
+		HttpServletResponse response=servletWebRequest.getResponse();
 		response.setStatus(HttpServletResponse.SC_ACCEPTED);
     }
 	
 	@AfterThrowing("execution(* com..*(..))")
     public void fail(JoinPoint joinPoint) throws Throwable {
-		HttpServletResponse response = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getResponse();
-		response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		System.out.println("@#!@#!#!@#");
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+		ServletWebRequest servletWebRequest=new ServletWebRequest(request);
+		HttpServletResponse response=servletWebRequest.getResponse();
+		response.setStatus(100);
     }
 */
 }
