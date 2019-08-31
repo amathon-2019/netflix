b7a65bd0caf8f22810760b565a88b99886db998e
lebmouse <gtah2yk@gmail.com>
2019-08-31 16:45:58 +0900

    element-ui 공통 설정 추가

--- 4flix/src/main.js
+++ 4flix/src/main.js
@@ -1,12 +1,22 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
 Vue.config.productionTip = false;
+
+// element-ui config
+// configure language
+import lang from "element-ui/lib/locale/lang/ko";
+import locale from "element-ui/lib/locale";
+locale.use(lang);
+// components
+import { Button, Input } from "element-ui";
+Vue.component(Button.name, Button);
+Vue.component(Input.name, Input);
 
 new Vue({
   router,
   store,
-  render: h => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount("#app");
