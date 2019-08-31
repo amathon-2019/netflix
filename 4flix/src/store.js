337cbd4f37f463f6eceb9bdd53e46370a741a38d
lebmouse <gtah2yk@gmail.com>
2019-09-01 02:21:31 +0900

    Home 상태관리 완료

--- 4flix/src/store.js
+++ 4flix/src/store.js
@@ -1,16 +1,38 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "./utill/axios";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    userData: false,
+    token: false
+  },
+  getters: {
+    getUser(state) {
+      return state.userData;
+    },
+    getToken(state) {
+      return state.token;
+    }
   },
   mutations: {
-
+    setUser(state, payload) {
+      state.userData = payload;
+    },
+    setToken(state, payload) {
+      state.token = payload;
+    },
   },
   actions: {
-
-  },
+    login({ commit }, payload) {
+      axios.post("/login", payload).then(res => {
+        commit("setUser", res.data.user);
+        commit("setToken", res.data.token);
+      });
+    },
+    logout({ commit }) {
+      commit("setUser", false);
+    }
+  }
 });
--- 4flix/src/views/Home.vue
+++ 4flix/src/views/Home.vue
@@ -11,10 +11,16 @@
       </vue-glide-slide>
     </vue-glide>
     <div class="home-form">
-      <el-input placeholder="아이디" v-model="email"></el-input>
-      <el-input placeholder="비밀번호" v-model="password"></el-input>
-      <el-button @click="login">로그인</el-button>
-      <el-button class="gray">회원가입</el-button>
+      <div class="home-form-container" v-if="!getUser">
+        <el-input placeholder="아이디" v-model="email"></el-input>
+        <el-input placeholder="비밀번호" v-model="password" show-password></el-input>
+        <el-button @click="login" v-text="isLoading?'loading...':'로그인'"></el-button>
+        <el-button @click="goSignUp" class="gray" v-text="'회원가입'"></el-button>
+      </div>
+      <div class="home-form-container" v-else>
+        <el-button @click="goMyPage" v-text="'마이페이지'"></el-button>
+        <el-button class="gray" @click="logout" v-text="'로그아웃'"></el-button>
+      </div>
     </div>
   </div>
 </template>
@@ -23,6 +29,7 @@
 import { Glide, GlideSlide } from 'vue-glide-js';
 import 'vue-glide-js/dist/vue-glide.css';
 const slideImg = require(`@/assets/image/slide.png`);
+import { mapGetters } from 'vuex';
 export default {
   name: 'home',
   data() {
@@ -31,6 +38,7 @@ export default {
       glideOption: { perView: 1, autoplay: 3000 },
       email: '',
       password: '',
+      isLoading: false,
       slideOption: [
         '4FLIX 커뮤니티는 한국 넷플릭스 사용자 커뮤니티입니다.',
         '4FLIX는 한 달마다 새로운 사용자들과 매칭시켜드립니다.',
@@ -41,12 +49,53 @@ export default {
       ]
     };
   },
+  watch: {
+    getUser(val, oldVal) {
+      console.log('hi', val);
+      return val;
+    }
+  },
+  computed: {
+    mainButtonText() {
+      if (this.getUser) {
+        return '마이페이지';
+      } else {
+        return '로그인';
+      }
+    },
+    ...mapGetters(['getUser'])
+  },
   components: {
     [Glide.name]: Glide,
     [GlideSlide.name]: GlideSlide
   },
   methods: {
-    login() {}
+    goMyPage() {
+      this.$router.push({ name: 'mypage' });
+    },
+    goSignUp() {
+      this.$router.push({ name: 'signup' });
+    },
+    login() {
+      this.isLoading = true;
+      setTimeout(this.signup, 1000);
+    },
+    logout() {
+      this.$store.dispatch('logout');
+    },
+    signup() {
+      this.$store
+        .dispatch('login', {
+          email: this.email,
+          password: this.password
+        })
+        .then(res => {
+          this.isLoading = false;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
   }
 };
 </script>
@@ -71,11 +120,13 @@ export default {
     }
   }
   &-form {
-    display: flex;
-    margin: auto;
-    flex-direction: column;
-    width: 70%;
-    max-width: 800px;
+    &-container {
+      display: flex;
+      margin: auto;
+      flex-direction: column;
+      width: 70%;
+      max-width: 800px;
+    }
     .el-button {
       margin-bottom: 10px;
       font-size: 17px;
