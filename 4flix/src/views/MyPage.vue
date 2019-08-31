8e3002a76f8b4425cc99a9de902517ee808ef9e1
lebmouse <gtah2yk@gmail.com>
2019-08-31 23:34:06 +0900

    카카오 결제 테스트 만듬

--- 4flix/src/views/MyPage.vue
+++ 4flix/src/views/MyPage.vue
@@ -16,7 +16,7 @@
         </div>
         <div class="myPage-netflix isPayment" v-else>
           <div class="myPage-netflix-desc">결제하지 않은 회원입니다.</div>
-          <el-button class="gray">결제하기</el-button>
+          <el-button class="gray" @click="requestPay">결제하기</el-button>
         </div>
       </div>
       <div class="myPage-password">
@@ -35,6 +35,7 @@
 </template>
 
 <script>
+const IMP = window.IMP;
 export default {
   data() {
     return {
@@ -43,8 +44,63 @@ export default {
       netflixPw: 'gtah2yk',
       prevPassword: '',
       newPassword: '',
-      newPassword2: ''
+      newPassword2: '',
+      uid: 2,
+      uemail: 'gtah2yk@gmail.com'
     };
+  },
+  methods: {
+    requestPay() {
+      IMP.request_pay(
+        {
+          pg: 'kakaopay',
+          pay_method: 'card',
+          merchant_uid: 'merchant_' + new Date().getTime(),
+          name: '주문명:결제테스트',
+          amount: 4000,
+          buyer_email: this.uemail,
+          buyer_tel: '010-1234-5678',
+        },
+        function(rsp) {
+          if (rsp.success) {
+            //[1] 서버단에서 결제정보 조회를 위해 jQuery ajax로 imp_uid 전달하기
+            jQuery
+              .ajax({
+                url: '/payments/complete', //cross-domain error가 발생하지 않도록 주의해주세요
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                  imp_uid: rsp.imp_uid
+                  //기타 필요한 데이터가 있으면 추가 전달
+                }
+              })
+              .done(function(data) {
+                //[2] 서버에서 REST API로 결제정보확인 및 서비스루틴이 정상적인 경우
+                if (everythings_fine) {
+                  var msg = '결제가 완료되었습니다.';
+                  msg += '\n고유ID : ' + rsp.imp_uid;
+                  msg += '\n상점 거래ID : ' + rsp.merchant_uid;
+                  msg += '결제 금액 : ' + rsp.paid_amount;
+                  msg += '카드 승인번호 : ' + rsp.apply_num;
+
+                  alert(msg);
+                } else {
+                  //[3] 아직 제대로 결제가 되지 않았습니다.
+                  //[4] 결제된 금액이 요청한 금액과 달라 결제를 자동취소처리하였습니다.
+                }
+              });
+          } else {
+            var msg = '결제에 실패하였습니다.';
+            msg += '에러내용 : ' + rsp.error_msg;
+
+            alert(msg);
+          }
+        }
+      );
+    }
+  },
+  mounted() {
+    IMP.init('imp52759429');
   }
 };
 </script>
