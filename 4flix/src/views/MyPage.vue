6fe87ba90312f61f1d74135b3faa49d3da92587e
lebmouse <gtah2yk@gmail.com>
2019-09-01 04:00:06 +0900

    결제하기 완료

--- 4flix/src/views/MyPage.vue
+++ 4flix/src/views/MyPage.vue
@@ -37,6 +37,7 @@
 <script>
 const IMP = window.IMP;
 import { mapGetters } from 'vuex';
+import axios from 'axios';
 export default {
   data() {
     return {
@@ -45,8 +46,7 @@ export default {
       netflixPw: '',
       prevPassword: '',
       newPassword: '',
-      newPassword2: '',
-      uemail: 'gtah2yk@gmail.com'
+      newPassword2: ''
     };
   },
   computed: {
@@ -61,37 +61,40 @@ export default {
           merchant_uid: 'merchant_' + new Date().getTime(),
           name: '주문명:결제테스트',
           amount: 4000,
-          buyer_email: this.uemail,
+          buyer_email: this.getUser.email,
           buyer_tel: '010-1234-5678'
         },
-        function(rsp) {
+        rsp => {
+          console.log(rsp);
           if (rsp.success) {
             //[1] 서버단에서 결제정보 조회를 위해 jQuery ajax로 imp_uid 전달하기
-            jQuery
-              .ajax({
-                url: '/payments/complete', //cross-domain error가 발생하지 않도록 주의해주세요
-                type: 'POST',
-                dataType: 'json',
-                data: {
-                  imp_uid: rsp.imp_uid
-                  //기타 필요한 데이터가 있으면 추가 전달
-                }
-              })
-              .done(function(data) {
-                //[2] 서버에서 REST API로 결제정보확인 및 서비스루틴이 정상적인 경우
-                if (everythings_fine) {
+            console.log(this.getUser.id);
+            axios({
+              url: 'http://13.124.122.246:8080/pay', //cross-domain error가 발생하지 않도록 주의해주세요
+              method: 'post',
+              headers: {
+                'content-type': 'application/json'
+              },
+              data: {
+                id: this.getUser.id
+              }
+            })
+              .then(res => {
+                console.log(res);
+                this.netflixEmail = res.data.email;
+                this.netflixPw = res.data.password;
+                if (res.status == 202) {
                   var msg = '결제가 완료되었습니다.';
                   msg += '\n고유ID : ' + rsp.imp_uid;
                   msg += '\n상점 거래ID : ' + rsp.merchant_uid;
-                  msg += '결제 금액 : ' + rsp.paid_amount;
-                  msg += '카드 승인번호 : ' + rsp.apply_num;
-
+                  msg += '\n결제 금액 : ' + rsp.paid_amount;
                   alert(msg);
                 } else {
                   //[3] 아직 제대로 결제가 되지 않았습니다.
                   //[4] 결제된 금액이 요청한 금액과 달라 결제를 자동취소처리하였습니다.
                 }
-              });
+              })
+              .catch(err => console.log(err));
           } else {
             var msg = '결제에 실패하였습니다.';
             msg += '에러내용 : ' + rsp.error_msg;
