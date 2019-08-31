45bbb6085ff5917c2db655f2daf2cb8d2e99d7dd
skyil7 <giopaik@naver.com>
2019-08-31 16:55:36 +0900

    EmailSender 이메일 발송 기능 구현

--- netflix/src/main/java/com/example/netflix/NetflixApplication.java
+++ netflix/src/main/java/com/example/netflix/NetflixApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NetflixApplication {
-
+    
 	public static void main(String[] args) {
 		SpringApplication.run(NetflixApplication.class, args);
 	}
